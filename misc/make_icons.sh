c061570f8028b28df06d4d45d3f30fdacfa66008
Jaycie Ewald <jaycie.ewald@outlook.com>
2023-12-06 09:24:35 -0600

    icons generation script

---
+++ misc/make_icons.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# root name
+SRC=rott
+
+# make ico
+convert -background transparent -define 'icon:auto-resize=16,24,32,64' ${SRC}.svg ${SRC}.ico
+
+# make pngs
+for (( i=16; i<=256; i*=2 ))
+do
+	inkscape -w $i -h $i ${SRC}.svg -o ${SRC}_${i}x${i}.png
+done
