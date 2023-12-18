2f4caced61680ba9ae02d8062580d3f2abbea3b6
Jaycie Ewald <jaycie.ewald@outlook.com>
2023-12-17 22:01:16 -0600

    rt_vid.x Start

---
+++ source/rt_vidx.c
@@ -0,0 +1,138 @@
+/*
+Copyright (C) 1994-1995 Apogee Software, Ltd.
+Copyright (C) 2002-2015 Steven Fuller, Ryan C. Gordon, John Hall, Dan Olson
+Copyright (C) 2023 Fabian Greffrath
+Copyright (C) 2023 erysdren (it/she/they)
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <string.h>
+
+#include "rt_view.h"
+#include "rt_vidx.h"
+
+/* global variables */
+SDL_Surface *WorldCanvas = NULL;
+SDL_Surface *OverlayCanvas = NULL;
+vxconfig_t VX_Config = {
+	CANVAS_WIDTH,
+	CANVAS_HEIGHT,
+	CANVAS_SCALE_MIN,
+	true
+};
+
+/* local variables */
+static SDL_Window *Window = NULL;
+static SDL_Renderer *Renderer = NULL;
+static SDL_Texture *RenderTexture = NULL;
+static SDL_Surface *RenderSurface = NULL;
+static SDL_Rect WorldCanvasRect = {0, 0, 0, 0};
+static SDL_Rect OverlayCanvasRect = {0, 0, 0, 0};
+
+/* initialize video subsystem */
+void VX_Init(void)
+{
+	unsigned int pixel_format, rmask, gmask, bmask, amask;
+	int bpp;
+
+	/* init sdl subsystems */
+	SDL_InitSubSystem(SDL_INIT_VIDEO | SDL_INIT_AUDIO);
+
+	/* create world canvas */
+	WorldCanvas = SDL_CreateRGBSurface(0, VX_Config.CanvasWidth * VX_Config.WorldCanvasScale, VX_Config.CanvasHeight * VX_Config.WorldCanvasScale, 8, 0, 0, 0, 0);
+
+	/* create overlay canvas */
+	OverlayCanvas = SDL_CreateRGBSurface(0, VX_Config.CanvasWidth, VX_Config.CanvasHeight, 8, 0, 0, 0, 0);
+	SDL_SetColorKey(OverlayCanvas, SDL_TRUE, 255);
+
+	/* create window */
+	Window = SDL_CreateWindow(PACKAGE_STRING, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WorldCanvas->w, WorldCanvas->h, SDL_WINDOW_RESIZABLE | SDL_WINDOW_ALLOW_HIGHDPI);
+	SDL_SetWindowMinimumSize(Window, WorldCanvas->w, WorldCanvas->h);
+
+	/* create renderer */
+	SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "nearest");
+	Renderer = SDL_CreateRenderer(Window, -1, SDL_RENDERER_PRESENTVSYNC);
+	SDL_SetRenderDrawColor(Renderer, 0, 0, 0, 255);
+	SDL_RenderClear(Renderer);
+	SDL_RenderPresent(Renderer);
+
+	/* setup stretch */
+	if (VX_Config.WindowStretchAspect)
+		SDL_RenderSetLogicalSize(Renderer, WorldCanvas->w, WorldCanvas->h * 1.2f);
+	else
+		SDL_RenderSetLogicalSize(Renderer, WorldCanvas->w, WorldCanvas->h);
+
+	/* get pixel format from screen */
+	pixel_format = SDL_GetWindowPixelFormat(Window);
+	SDL_PixelFormatEnumToMasks(pixel_format, &bpp, &rmask, &gmask, &bmask, &amask);
+
+	/* create render surface */
+	RenderSurface = SDL_CreateRGBSurface(0, WorldCanvas->w, WorldCanvas->h, bpp, rmask, gmask, bmask, amask);
+
+	/* create render texture */
+	RenderTexture = SDL_CreateTexture(Renderer, pixel_format, SDL_TEXTUREACCESS_STREAMING, WorldCanvas->w, WorldCanvas->h);
+}
+
+/* shutdown video subsystem */
+void VX_Shutdown(void)
+{
+	if (Window) SDL_DestroyWindow(Window);
+	if (Renderer) SDL_DestroyRenderer(Renderer);
+	if (RenderTexture) SDL_DestroyTexture(RenderTexture);
+	if (RenderSurface) SDL_FreeSurface(RenderSurface);
+	if (WorldCanvas) SDL_FreeSurface(WorldCanvas);
+	if (OverlayCanvas) SDL_FreeSurface(OverlayCanvas);
+}
+
+/* flip all canvas changes to visible screen */
+void VX_Flip(void)
+{
+	WorldCanvasRect.x = 0;
+	WorldCanvasRect.y = 0;
+	WorldCanvasRect.w = WorldCanvas->w;
+	WorldCanvasRect.h = WorldCanvas->h;
+
+	OverlayCanvasRect.x = 0;
+	OverlayCanvasRect.y = 0;
+	OverlayCanvasRect.w = OverlayCanvas->w;
+	OverlayCanvasRect.h = OverlayCanvas->h;
+
+	SDL_LowerBlit(WorldCanvas, &WorldCanvasRect, RenderSurface, &WorldCanvasRect);
+	SDL_LowerBlit(OverlayCanvas, &OverlayCanvasRect, RenderSurface, &OverlayCanvasRect);
+	SDL_UpdateTexture(RenderTexture, NULL, RenderSurface->pixels, RenderSurface->pitch);
+	SDL_RenderClear(Renderer);
+	SDL_RenderCopy(Renderer, RenderTexture, NULL, NULL);
+	SDL_RenderPresent(Renderer);
+}
+
+/* set video palette */
+void VX_SetPalette(uint8_t *palette)
+{
+	SDL_Color colors[256];
+	int i;
+
+	for (i = 0; i < 256; i++)
+	{
+		colors[i].r = gammatable[(gammaindex << 6) + (*palette++)] << 2;
+		colors[i].g = gammatable[(gammaindex << 6) + (*palette++)] << 2;
+		colors[i].b = gammatable[(gammaindex << 6) + (*palette++)] << 2;
+	}
+
+	SDL_SetPaletteColors(OverlayCanvas->format->palette, colors, 0, 256);
+	SDL_SetPaletteColors(WorldCanvas->format->palette, colors, 0, 256);
+}
---
+++ source/rt_vidx.h
@@ -0,0 +1,97 @@
+/*
+Copyright (C) 1994-1995 Apogee Software, Ltd.
+Copyright (C) 2002-2015 Steven Fuller, Ryan C. Gordon, John Hall, Dan Olson
+Copyright (C) 2023 Fabian Greffrath
+Copyright (C) 2023 erysdren (it/she/they)
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#ifndef _rt_vidx_public
+#define _rt_vidx_public
+
+//***************************************************************************
+//
+// Public header for RT_VIDX.C
+//
+//***************************************************************************
+
+#include <stdbool.h>
+#include <stdint.h>
+#include "SDL.h"
+
+//***************************************************************************
+//
+// DEFINES
+//
+//***************************************************************************
+
+#define CANVAS_SCALE_MIN (1)
+#define CANVAS_SCALE_MAX (4)
+#define CANVAS_WIDTH (320)
+#define CANVAS_HEIGHT (200)
+#define CANVAS_BPP (8)
+#define CANVAS_WIDTH_MIN (CANVAS_WIDTH * CANVAS_SCALE_MIN)
+#define CANVAS_HEIGHT_MIN (CANVAS_HEIGHT * CANVAS_SCALE_MIN)
+#define CANVAS_WIDTH_MAX (CANVAS_WIDTH * CANVAS_SCALE_MAX)
+#define CANVAS_HEIGHT_MAX (CANVAS_HEIGHT * CANVAS_SCALE_MAX)
+
+/* get or set overlay canvas pixel */
+#define OverlayCanvasPixel(x, y) ((uint8_t *)OverlayCanvas->pixels)[y * OverlayCanvas->pitch + x]
+
+/* get or set world canvas pixel */
+#define WorldCanvasPixel(x, y) ((uint8_t *)WorldCanvas->pixels)[y * WorldCanvas->pitch + x]
+
+//***************************************************************************
+//
+// TYPES
+//
+//***************************************************************************
+
+typedef struct vxconfig_t {
+	int CanvasWidth;
+	int CanvasHeight;
+	int WorldCanvasScale;
+	bool WindowStretchAspect;
+} vxconfig_t;
+
+//***************************************************************************
+//
+// GLOBALS
+//
+//***************************************************************************
+
+extern SDL_Surface *WorldCanvas;
+extern SDL_Surface *OverlayCanvas;
+extern vxconfig_t VX_Config;
+
+//***************************************************************************
+//
+// PROTOTYPES
+//
+//***************************************************************************
+
+/* initialize video subsystem */
+void VX_Init(void);
+
+/* shutdown video subsystem */
+void VX_Shutdown(void);
+
+/* flip all canvas changes to visible screen */
+void VX_Flip(void);
+
+/* set video palette */
+void VX_SetPalette(uint8_t *palette);
+
+#endif /* _rt_vidx_public */
