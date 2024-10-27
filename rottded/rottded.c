fc349b93ecc14e59a7330a8d14e87a9b7d5f8239
erysdren <contact@erysdren.me>
2024-10-27 01:12:50 -0500

    rottded: basic setup

--- rottded/rottded.c
+++ rottded/rottded.c
@@ -7,6 +7,9 @@
 #define SDL_MAIN_USE_CALLBACKS
 #include <SDL3/SDL_main.h>
 
+static SDLNet_DatagramSocket *socket = NULL;
+static Uint16 port = 34858;
+
 static void log_func(void *userdata, int category, SDL_LogPriority priority, const char *message)
 {
 	Uint64 when;
@@ -29,8 +32,38 @@ static void log_func(void *userdata, int category, SDL_LogPriority priority, con
 	fprintf(stderr, "[%09.4f] %s%s\n", seconds, priorities[priority], message);
 }
 
+static int check_arg(int argc, char **argv, const char *arg)
+{
+	for (int i = 0; i < argc; i++)
+	{
+		/* check if arg is there */
+		if (SDL_strcmp(argv[i], arg) == 0)
+			return 1;
+	}
+
+	return 0;
+}
+
+static const char *get_arg(int argc, char **argv, const char *arg)
+{
+	for (int i = 0; i < argc; i++)
+	{
+		/* check if arg is there */
+		if (SDL_strcmp(argv[i], arg) != 0)
+			continue;
+
+		/* check if it has a value */
+		if (argc > (i + 1))
+			return argv[i + 1];
+	}
+
+	return NULL;
+}
+
 SDL_AppResult SDL_AppInit(void **appstate, int argc, char **argv)
 {
+	const char *arg;
+
 	/* set log func */
 	SDL_SetLogOutputFunction(log_func, NULL);
 
@@ -50,17 +83,40 @@ SDL_AppResult SDL_AppInit(void **appstate, int argc, char **argv)
 
 	SDL_Log("SDLNet: Initialized");
 
+	/* process args */
+	if ((arg = get_arg(argc, argv, "--port")) != NULL)
+		port = SDL_atoi(arg);
+
+	/* create socket */
+	socket = SDLNet_CreateDatagramSocket(NULL, port);
+	if (!socket)
+	{
+		SDL_Log("SDLNet: %s", SDL_GetError());
+		return SDL_APP_FAILURE;
+	}
+
+	SDL_Log("SDLNet: Listening on port %d", port);
+
 	return SDL_APP_CONTINUE;
 }
 
 void SDL_AppQuit(void *appstate, SDL_AppResult result)
 {
+	SDLNet_DestroyDatagramSocket(socket);
 	SDLNet_Quit();
 	SDL_Quit();
 }
 
 SDL_AppResult SDL_AppIterate(void *appstate)
 {
+	SDLNet_Datagram *datagram = NULL;
+
+	while ((SDLNet_ReceiveDatagram(socket, &datagram) == true) && (datagram != NULL))
+	{
+		SDL_Log("Got %d-byte datagram from %s:%d", datagram->buflen, SDLNet_GetAddressString(datagram->addr), datagram->port);
+		SDLNet_DestroyDatagram(datagram);
+	}
+
 	return SDL_APP_CONTINUE;
 }
 
